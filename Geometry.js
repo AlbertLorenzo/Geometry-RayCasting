497a3db4c5960d5697fd39bad8419fc35eba4104
AlbertLorenzo <albertlorenzofc@gmail.com>
2019-05-09 11:41:50 +0200

    Geometry

---
+++ Geometry.js
@@ -0,0 +1,22 @@
+function setup() {
+    // Crea un plano 2d 400 ancho * 400 largo de color negro
+    createCanvas(400, 400)
+}
+
+const vector = {
+    x: 200,
+    y: 200
+}
+
+// No para de dibujar
+function draw() {
+    background(0)
+
+    // Cast rayos
+    for (let i = 1; i < 400; i++) {
+        for (let k = 1; k < 500; k += 75) {
+            stroke('rgba(255, 255, 255, .007750)')
+            line(vector.x, vector.y, k, i)
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Geometry
+Jugando con la librería p5js - geometría
+
+[alpha=0.025]
+[alpha=0.075]
+[alpha=0.250]
+
+Resultados:
+
+![alpha=0.025](https://github.com/AlbertLorenzo/Geomtry-p5/blob/master/pntScreen/.0250.PNG)
+![alpha=0.075](https://github.com/AlbertLorenzo/Geomtry-p5/blob/master/pntScreen/.0750.PNG)
+![alpha=0.250](https://github.com/AlbertLorenzo/Geomtry-p5/blob/master/pntScreen/100-.250.PNG)
